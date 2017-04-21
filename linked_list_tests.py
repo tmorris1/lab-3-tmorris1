5aee596d80234e92173fe7749cb7e24b1775b26c
John Clements <clements@racket-lang.org>
2017-04-21 15:34:57 -0700

    added template test files

---
+++ linked_list_tests.py
@@ -0,0 +1,16 @@
+import unittest
+from linked_list import *
+
+class TestList(unittest.TestCase):
+    # Note that this test doesn't assert anything! It just verifies your
+    #  class and function definitions.
+    def test_interface(self):
+        temp_list = empty_list()
+        temp_list = add(temp_list, 0, "Hello!")
+        length(temp_list)
+        get(temp_list, 0)
+        temp_list = set(temp_list, 0, "Bye!")
+        remove(temp_list, 0)
+
+if __name__ == '__main__':
+    unittest.main()
